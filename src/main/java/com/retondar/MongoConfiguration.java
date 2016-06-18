feb154a1644ca6ba4dc467376a5990b5604d8980
Thiago Retondar <t.retondar@gmail.com>
2016-06-18 12:04:48 -0300

    Trocando .properties por .yml e organizando configuração do Mongodb

---
+++ src/main/java/com/retondar/MongoConfiguration.java
@@ -0,0 +1,34 @@
+package com.retondar;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.convert.*;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+/**
+ * Created by thiagoretondar on 18/06/16.
+ */
+@Configuration
+public class MongoConfiguration {
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDbFactory factory, MongoMappingContext context, BeanFactory beanFactory) {
+
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+        MappingMongoConverter mappingConverter = new MappingMongoConverter(dbRefResolver, context);
+        try {
+            mappingConverter.setCustomConversions(beanFactory.getBean(CustomConversions.class));
+        } catch (NoSuchBeanDefinitionException ignore) {
+        }
+
+        // Don't save _class to mongo
+        mappingConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return mappingConverter;
+    }
+
+
+}
