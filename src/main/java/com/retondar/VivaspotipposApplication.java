9cc95f9038e2c4c301038699bd824cd15ab844cf
Thiago Retondar <t.retondar@gmail.com>
2016-06-18 11:27:58 -0300

    Initial commit

---
+++ src/main/java/com/retondar/VivaspotipposApplication.java
@@ -0,0 +1,12 @@
+package com.retondar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaspotipposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivaspotipposApplication.class, args);
+	}
+}
